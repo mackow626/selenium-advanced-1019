647e80e968c7efbd5d8f22fa83cbba64db8a2685
Mateusz Tadla <mtadla@sii.pl>
2019-10-27 11:48:01 +0100

    add miniture page eleemts

--- src/main/java/pages/ProductMiniaturePage.java
+++ src/main/java/pages/ProductMiniaturePage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
 
@@ -10,4 +11,23 @@ public class ProductMiniaturePage extends BasePage {
         super(driver);
         PageFactory.initElements(new DefaultElementLocatorFactory(element), this);
     }
+    
+    @FindBy(className = "price")
+    private WebElement price;
+
+    @FindBy(className = "regular-price")
+    private WebElement priceBeforeDiscount;
+
+    @FindBy(className = "product-title")
+    private WebElement name;
+
+    @FindBy(className = "new")
+    private WebElement newLabel;
+
+    @FindBy(css = "img")
+    private WebElement image;
+
+    @FindBy(className = "discount-product")
+    private WebElement discountLabel;
+
 }
